1f6477c6199fcbc7225a5fe18429502f0bfdba2c
Anna Weisbrodt <aweisbro@gmail.com>
2016-08-02 11:52:19 -0600

    services spec passing

--- spec/services/bestbuy_service_spec.rb
+++ spec/services/bestbuy_service_spec.rb
@@ -4,10 +4,18 @@ describe "BestbuyService" do
   it "gets stores" do
     VCR.use_cassette("get-stores") do
       service = BestbuyService.new
+      result = service.get_stores("80205")
 
-      response = service.get_stores("80205")
+      expect(result['stores'].count).to eq(15)
+    end
+  end
 
-    
+  it "gets one store" do
+    VCR.use_cassette("get-store") do
+      service = BestbuyService.new
+      result = service.get_store("2740")
+
+      expect(result['stores'].first['city']).to eq("Denver")
     end
   end
 end
